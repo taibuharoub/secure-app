6ff86bcdd08bf4da64aa0335a997373e6608f24d
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-04 19:30:11 +0300

    Add error handling middleware

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json({limit: "1mb"}));
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Preparing your Code for Production 👩‍💻👨‍💻👨‍💻"
-    })
-})
+  res.status(200).json({
+    message: "Preparing your Code for Production 👩‍💻👨‍💻👨‍💻",
+  });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-})
+  console.log(`Server started at http://localhost:${port}`);
+});
