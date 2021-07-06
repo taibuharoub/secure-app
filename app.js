1ec7424e74e4b7ee4b3b92f8d64f993e0c107bf7
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-07 01:49:23 +0300

    Add Sentry logic

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
 const morgan = require("morgan");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
 
 const { accessLogStream } = require("./helpers/logging");
 require("dotenv").config();
@@ -16,6 +18,16 @@ const privateKey = fs.readFileSync("server.key");
 const certificate = fs.readFileSync("server.cert");
 
 app.use(express.json({ limit: "1mb" }));
+Sentry.init({
+  dsn: process.env.SENTRY_URL,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+})
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(helmet());
 app.use(compression());
@@ -26,6 +38,22 @@ app.get("/", (req, res, next) => {
     message: "Preparing your Code for Production 👩‍💻👨‍💻👨‍💻",
   });
 });
+
+app.post("/", (req, res, next) => {
+  const {name} = req.body;
+  if(!name) {
+    const error = new Error("Name is Required.");
+    error.statusCode = 404;
+    throw error;
+  }
+  // doSomething(); //tigger error for Sentry
+  res.status(200).json({
+    success: true,
+    name
+  })
+})
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -38,3 +66,8 @@ app.use((error, req, res, next) => {
 https.createServer({key: privateKey, cert: certificate}, app).listen(port, () => {
   console.log(`Server started at https://localhost:${port}`);
 });
+
+/* app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+}); */
+
